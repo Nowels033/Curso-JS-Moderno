1ce78286203837524e509e8a50b160f919be4f1f
Nowels033 <noeldomse@gmail.com>
2024-11-23 23:38:34 +0100

    Tema 6

---
+++ 06-Booleans/js/01-app.js
@@ -0,0 +1,20 @@
+const boolean1 = true;
+const boolean2 = false;
+const boolean3 = "true";
+const boolean4 = new Boolean(true);
+
+console.log(boolean1);
+console.log(boolean2);
+console.log(boolean3);
+
+console.log(typeof boolean1);
+console.log(typeof boolean2);
+console.log(typeof boolean3);
+console.log(typeof boolean4);
+
+
+console.log(boolean1 == boolean2);
+console.log(boolean1 === boolean2);
+
+console.log(boolean1 == boolean3);
+console.log(boolean1 === boolean3);
---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,12 @@
+const autenticado = true;
+
+if(autenticado){
+    console.log("Usuario autenticado");
+}else{
+    console.log("Usuario no autenticado");
+}
+
+//operador ternario
+
+console.log(autenticado ? "Usuario autenticado" : "Usuario no autenticado");
+
